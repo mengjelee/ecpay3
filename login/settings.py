be112a6471690b2311244d48f1ae41073bedddba
mengjelee <43258760+mengjelee@users.noreply.github.com>
2021-03-07 00:06:39 +0800

    210307

--- login/settings.py
+++ login/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.CatalogConfig', 
+    'catalog.apps.CatalogConfig',
+    'fontawesome-free', 
 ]
 
 MIDDLEWARE = [
